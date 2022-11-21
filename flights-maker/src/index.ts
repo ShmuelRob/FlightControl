b85c97f6a5c05c2e335582c5a9c4d6493c1027e2
Shmuel Robinov <shmuel.robinov@gmail.com>
2022-11-21 15:11:20 +0200

    finish the project

--- flights-maker/src/index.ts
+++ flights-maker/src/index.ts
@@ -1,21 +1,8 @@
 import { Server } from 'socket.io';
 import config from './custom-environment-variables.json';
-import dotenv from 'dotenv';
-
-// dotenv.config({
-    // path: `./env.${process.env.NODE_ENV? process.env.NODE_ENV : ''}`,
-// });
 
 let idCounter = 0;
 const socket = new Server(config.FLIGHT_MAKER_PORT, { cors: { origin: '*', methods: ['GET'] } });
-// const socket = new Server(parseInt(process.env.FLIGHT_MAKER_PORT || '7000'), { cors: { origin: '*', methods: ['GET'] } });
-// const socket = new Server(parseInt(process.env.FLIGHT_MAKER_PORT!), { cors: { origin: '*', methods: ['GET'] } });
-
-
-socket.on('connection', () => {
-    console.log('connected');
-    createFlights();
-});
 
 const setBool = (): boolean => {
     return Math.floor(Math.random() * 100) % 2 === 0;
